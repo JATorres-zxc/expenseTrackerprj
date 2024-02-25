7103359af9ce726b8d8b7a8f604c79ccf0ff1d9f
JATorres-zxc <angelotorres200425@gmail.com>
2024-02-25 23:48:35 +0800

    created balance,header,IncomeExpenses,TransactionList, setup the balance header IncomeExpenses

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,58 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Header />
+  <div class="container">
+    <Balance :total="total"/>
+    <IncomeExpenses :income="income" :expenses="expenses"/>
+    <TransactionList :transactions="transactions" />
+    <AddTransaction />
+  </div>  
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+
+
+
+
+<script setup>
+  import Header from './components/Header.vue';
+  import Balance from './components/Balance.vue';
+  import IncomeExpenses from './components/IncomeExpenses.vue';
+  import TransactionList from './components/TransactionList.vue';
+  import AddTransaction from './components/AddTransaction.vue';
+  
+
+  import {ref, computed} from 'vue'
+
+    // Define transactions
+    const transactions = ref([
+    { id: 1, text: 'hehe', amount: 100 },
+    { id: 2, text: 'hehehe', amount: -50 },
+    { id: 3, text: 'hehehehe', amount: 200 },
+    // Other transactions...
+  ]);
+  // get total
+  const total = computed(() => {
+    return transactions.value.reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0);
+  });
+
+  // get income
+  const income = computed(() => {
+    return transactions.value.filter(transaction => transaction.amount > 0)
+      .reduce((acc, transaction) => {
+        return acc + transaction.amount;
+      }, 0).toFixed(2);
+  });
+
+  //get expenses
+  const expenses = computed(() => {
+    return transactions.value.filter(transaction => transaction.amount < 0)
+      .reduce((acc, transaction) => {
+        return acc + transaction.amount;
+      }, 0).toFixed(2);
+  });
+</script>

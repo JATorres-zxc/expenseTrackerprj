172c721ed06ab6a6fc06bf1d132f3e222c1571b1
JATorres-zxc <angelotorres200425@gmail.com>
2024-02-26 17:09:50 +0800

    header p

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
     <h2 style="text-align: center;">Expense Tracker</h2>
-    <h2 style="text-align: center;">In progress(userauth)</h2>
+    <p style="text-align: center;">In progress(userauth)</p>
 </template>
 
